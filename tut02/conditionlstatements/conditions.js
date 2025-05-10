794a55ffd42a9493faf06940badeb3368c71ce91
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2025-05-10 16:56:30 +0530

    first commit

---
+++ tut02/conditionlstatements/conditions.js
@@ -0,0 +1,13 @@
+// let ans = Number(prompt("enter your number"))
+
+// if (isNaN(ans)) {
+//     console.log("wrong input");
+
+// } else if (ans >= 18) {
+
+//     console.log("you can vote");
+
+// } else {
+//     console.log("you cant vote");
+
+// }
