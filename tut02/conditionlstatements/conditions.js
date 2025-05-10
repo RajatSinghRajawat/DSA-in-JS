5bb79791ad933cf2e7063a92e778013f69baaec6
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2025-05-10 17:32:54 +0530

    bijali bill

--- tut02/conditionlstatements/conditions.js
+++ tut02/conditionlstatements/conditions.js
@@ -40,24 +40,49 @@
 
 // solve in more easy way 
 
-let amount = Number(prompt("enter your amount"));
-let discount = 0
+// let amount = Number(prompt("enter your amount"));
+// let discount = 0
 
-if (amount > 0 && amount <= 5000) {
-    discount = 0
+// if (amount > 0 && amount <= 5000) {
+//     discount = 0
 
+// }
+// else if (amount > 5000 && amount <= 7000) {
+//     discount = 5
+
+// } else if (amount > 7000 && amount <= 9000) {
+//     discount = 10
+// }
+// else if (amount > 9000) {
+//     discount = 20
+// } else {
+//     console.log("wrong amount");
+
+// }
+
+// console.log(amount - Math.floor((dis * amount) / 100));
+
+
+
+//bijali bill
+
+let unit = Number(prompt("enter your number"));
+
+
+let amount = 0;
+
+if (unit > 400) {
+    amount = (unit - 400) * 13
+    unit = 400
 }
-else if (amount > 5000 && amount <= 7000) {
-    discount = 5
-
-} else if (amount > 7000 && amount <= 9000) {
-    discount = 10
+if (unit > 200 && unit <= 400) {
+    amount += (unit - 200) * 8
+    unit = 200
 }
-else if (amount > 9000) {
-    discount = 20
-} else {
-    console.log("wrong amount");
-
+if (unit > 100 && unit <= 200) {
+    amount += (unit - 200) * 6
+    unit = 200
 }
 
-console.log(amount - Math.floor((dis * amount) / 100));
+amount += unit * 4  
+console.log(amount);
